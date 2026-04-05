5ad4bb4d3b896db6a4e777ecfc115b19219f65b7
Jignesh <jigneshgohil202021@gmail.com>
2026-04-05 23:02:42 +0530

    this is updated db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+console.log("🔍 DATABASE_URL:", process.env.DATABASE_URL); // ← ADD THIS
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
