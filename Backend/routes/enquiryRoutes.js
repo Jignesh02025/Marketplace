f1f98c3e6cb2468fd412837051f3c3839ba64b0c
Jignesh <jigneshgohil202021@gmail.com>
2026-04-12 21:24:59 +0530

    feat: add enquiry routes for listing and creating enquiries with authentication middleware

--- Backend/routes/enquiryRoutes.js
+++ Backend/routes/enquiryRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getEnquiries } = require("../controllers/enquiryController");
+const { getEnquiries, createEnquiry } = require("../controllers/enquiryController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/",authMiddleware, getEnquiries);
-router.post("/",authMiddleware, createEnquiry);
+router.get("/", authMiddleware, getEnquiries);
+router.post("/", authMiddleware, createEnquiry);
 
 module.exports = router;
